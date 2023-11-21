a6db034ba5ec97c6e3db5607eb5adb8479a41d56
Marcelo Ito <marcelo-ito@hotmail.com>
2023-11-20 23:32:51 -0300

    Adapta @Gallery

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import { Container, Row } from "react-bootstrap";
+import { Button, ButtonGroup, Container, Row } from "react-bootstrap";
 import CustomCard from "../Card/Card";
 import "./Gallery.css";
 
@@ -6,9 +6,13 @@ export default function Gallery(props) {
   return (
     <app-gallery>
       <Container className="pt-4">
-      <div className="gallery-header">
-          <h1>Eu te amo, Bianca</h1>
+        <div className="gallery-header">
+          <h1>PARABÉNS PARA NÓS!</h1>
         </div>
+        {/* <ButtonGroup>
+          <Button variant="outline-dark">Versão Card</Button>
+          <Button variant="outline-dark">Versão Carrousel</Button>
+        </ButtonGroup> */}
         <Row className="row-cols-1 row-cols-sm-2 row-cols-md-3">
           {props.images.map((e, i) => (
             <CustomCard key={e.id} src={e.src} />
